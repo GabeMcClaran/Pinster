930f8a8f17f8c5bf5745746ad298b4f97b5ce6cd
GabeMcClaran <gabemcclaran@gmail.com>
2020-02-07 14:04:15 -0700

    first commit hooks up to tester...

--- wwwsrc/src/authConfig.js
+++ wwwsrc/src/authConfig.js
@@ -1,3 +1,3 @@
-export const domain = "";
-export const audience = "";
-export const clientId = "";
+export const domain = "gabemcclaran.auth0.com";
+export const audience = "https://bcw-project-api.com";
+export const clientId = "9D8bqHaRRyoEMmOI8Cu7FkuCgCl78VHo";
